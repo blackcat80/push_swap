9e641bc26d4da9e47f82439d90d7b37510025bb2
Christian SB <csitja80@gmail.com>
2023-07-22 04:33:16 +0200

    update

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: csitja-b <csitja-b@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 18:56:10 by csitja-b          #+#    #+#             */
-/*   Updated: 2023/07/22 04:30:52 by csitja-b         ###   ########.fr       */
+/*   Updated: 2023/07/22 04:33:07 by csitja-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,39 +33,38 @@ typedef struct s_stack
 
 // Utils
 
-void    free_stack(t_stack **stack);
-void    exit_error(t_stack **stack_a, t_stack **stack_b);
-long int    ft_atoi(const char *str);
-void    ft_putstr(char *str);
-int     nb_abs(int nb);
+void		free_stack(t_stack **stack);
+void		exit_error(t_stack **stack_a, t_stack **stack_b);
+long int	ft_atoi(const char *str);
+void		ft_putstr(char *str);
+int			nb_abs(int nb);
 
 // Stack functions
 
-t_stack     *stack_new(int value);
-t_stack     *fill_stack_values(int ac, char **av);
-int         get_stack_size(t_stack *stack);
-t_stack     *get_stack_bottom(t_stack *stack);
-t_stack     *get_stack_before_bottom(t_stack *stack);
-void    stack_add_bottom(t_stack **stack, t_stack *new);
-void    assign_index(t_stack *stack_a, int stack_size);
-void    get_target_position(t_stack **a, t_stack **b);
-int     get_lowest_index_position(t_stack **stack);
+t_stack		*stack_new(int value);
+t_stack		*fill_stack_values(int ac, char **av);
+int			get_stack_size(t_stack	*stack);
+t_stack		*get_stack_bottom(t_stack *stack);
+t_stack		*get_stack_before_bottom(t_stack *stack);
+void		stack_add_bottom(t_stack **stack, t_stack *new);
+void		assign_index(t_stack *stack_a, int stack_size);
+void		get_target_position(t_stack **a, t_stack **b);
+int			get_lowest_index_position(t_stack **stack);
 
 // Checking the input
 
-int     is_correct_input(char **av, int ac);
-int     is_digit(char c);
-int     is_sign(char c);
+int			is_correct_input(char **av, int ac);
+int			is_digit(char c);
+int			is_sign(char c);
 
 // Sorting
 
-void    sort(t_stack **stack_a, t_stack **stack_b);
-void    tiny_sort(t_stack **stack);
-int     is_sorted(t_stack *stack);
-void    do_move(t_stack **a, t_stack **b, int cost_a, int cost_b);
-void    do_cheapest_move(t_stack **stack_a, t_stack **stack_b);
-void    get_cost(t_stack **stack_a, t_stack **stack_b);
-
+void		sort(t_stack **stack_a, t_stack **stack_b);
+void		tiny_sort(t_stack **stack);
+int			is_sorted(t_stack *stack);
+void		do_move(t_stack **a, t_stack **b, int cost_a, int cost_b);
+void		do_cheapest_move(t_stack **stack_a, t_stack **stack_b);
+void		get_cost(t_stack **stack_a, t_stack **stack_b);
 
 // Movements
 
